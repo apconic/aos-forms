a996d84498118b83cbfe9c8a7e40e37a76bc4715
Suhail Ansari <suhaila@apconic.com>
2017-01-13 18:27:30 +0530

    Removed some extra components from index.js which were removed.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,12 @@
 import DataField from './data-fields/data-field';
-import SimpleRowActionTable from './tables/simple-row-action-table';
-import SimpleTable from './/tables/simple-table';
-import ActionButton from './action-button';
-import Adder from './adder';
-import ApconicThemeOne from './apconic-themes';
-import AppHeader from './app-header';
-import BaseSubheader from './base-subheader';
-import ConfirmationDialog from './confirmation-dialog';
 import DynamicForm from './dynamic-form';
-import IconFactory from './icon-factory';
-import ImageDialog from './image-dialog';
-import LoadingSpinner from './loading-spinner';
-import MutableGridList from './mutable-grid-list';
-import Notification from './notification';
-import Panel from './panel';
-import PanelWithHeader from './panel-with-header';
-import Shortlist from './shortlist';
-import SimpleGridList from './simple-grid-list';
-import SimpleGridTile from './simple-grid-tile';
 import composeWithRedux from './composer/compose-with-redux';
 import FormState from './actions/form-state';
 import FormsReducer from './reducers/form-action-reducers';
 
 export {
   DataField,
-  SimpleRowActionTable,
-  SimpleTable,
-  ActionButton,
-  Adder,
-  ApconicThemeOne,
-  AppHeader,
-  BaseSubheader,
-  ConfirmationDialog,
   DynamicForm,
-  IconFactory,
-  LoadingSpinner,
-  MutableGridList,
-  Notification,
-  Panel,
-  PanelWithHeader,
-  Shortlist,
-  SimpleGridList,
-  SimpleGridTile,
   FormsReducer,
   FormState,
   composeWithRedux
